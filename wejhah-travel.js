1f0fc813dd191b249acb01556639122ceee39784
Bandar-Zuhair <my.github.websites@gmail.com>
2025-01-02 13:43:41 +0700

    Update wejhah-travel.js

--- wejhah-travel.js
+++ wejhah-travel.js
@@ -490,7 +490,7 @@ function openFullScreenImage(src, text) {
     const whatsappButton = document.createElement('a');
     whatsappButton.className = 'whatsapp_button';
     whatsappButton.innerText = 'إرسال هذا العرض';
-    whatsappButton.href = `https://wa.me/+966138358350?text=طلب%20حجز%20هذا%20العرض:%0Ahttps://mohammed-website.github.io/wejhahtravel/${encodeURIComponent(src)}`;
+    whatsappButton.href = `https://wa.me/+966138358350?text=طلب%20حجز%20هذا%20العرض:%0A%0Ahttps://mohammed-website.github.io/wejhahtravel/${encodeURIComponent(src)}`;
     fullScreenDiv.appendChild(whatsappButton);
 
     // Close on background click
